24f2fe185352bef2249677061a37f3f031cd7ccb
AminDannak <amin.dannak@gmail.com>
2024-09-25 11:55:16 +0330

    feat: add theme toggler to auth pages

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,13 @@
+import ThemeToggler from "@/components/theme-toggler";
 import React, { ReactNode } from "react";
 
 function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <div className="h-[100vh] flex items-center justify-center relative">
       {children}
+      <div className="absolute bottom-5 right-0 text-white">
+        <ThemeToggler />
+      </div>
     </div>
   );
 }
